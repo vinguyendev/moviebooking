00e7ec5088450bfef9a1ef5e13ffdb0019ae9b4d
Vi Nguyen <vinguyen.dev@gmai.com>
2020-05-30 00:51:30 +0700

    update database

--- movie_booking.sql
+++ movie_booking.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100411
  File Encoding         : 65001
 
- Date: 28/05/2020 00:56:22
+ Date: 30/05/2020 00:50:58
 */
 
 SET NAMES utf8mb4;
@@ -59,7 +59,7 @@ CREATE TABLE `booking_seats`  (
   `booking_id` int(10) NULL DEFAULT NULL,
   `seat_id` int(10) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of booking_seats
@@ -72,6 +72,9 @@ INSERT INTO `booking_seats` VALUES (5, 7, 42);
 INSERT INTO `booking_seats` VALUES (6, 7, 43);
 INSERT INTO `booking_seats` VALUES (7, 8, 12);
 INSERT INTO `booking_seats` VALUES (8, 8, 13);
+INSERT INTO `booking_seats` VALUES (9, 9, 21);
+INSERT INTO `booking_seats` VALUES (10, 9, 22);
+INSERT INTO `booking_seats` VALUES (11, 9, 23);
 
 -- ----------------------------
 -- Table structure for bookings
@@ -87,7 +90,7 @@ CREATE TABLE `bookings`  (
   `created_at` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `movie_id` int(10) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of bookings
@@ -99,6 +102,7 @@ INSERT INTO `bookings` VALUES (5, 1, 4, '20200527', 3, 0, '07:05:23 27/05/2020',
 INSERT INTO `bookings` VALUES (6, 1, 4, '20200527', 3, 0, '07:05:55 27/05/2020', 6);
 INSERT INTO `bookings` VALUES (7, 1, 4, '20200527', 3, 0, '07:05:22 27/05/2020', 6);
 INSERT INTO `bookings` VALUES (8, 1, 4, '20200527', 3, 0, '07:05:10 27/05/2020', 5);
+INSERT INTO `bookings` VALUES (9, 2, 4, '20200529', 4, 0, '12:05:04 29/05/2020', 5);
 
 -- ----------------------------
 -- Table structure for cinemas
@@ -134,12 +138,15 @@ CREATE TABLE `customers`  (
   `area` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of customers
 -- ----------------------------
 INSERT INTO `customers` VALUES (1, 'Nguyễn Văn A', '012345', NULL, NULL, '123');
+INSERT INTO `customers` VALUES (2, 'N V B', '123', '123@gmail.com', 'HN', '123');
+INSERT INTO `customers` VALUES (3, 'N V C', '0343', 'nvc@gmail.com', 'HCM', '123');
+INSERT INTO `customers` VALUES (4, 'N V D', '122334567', 'nvd@gmail.com', 'DN', '123');
 
 -- ----------------------------
 -- Table structure for movies
@@ -159,17 +166,18 @@ CREATE TABLE `movies`  (
   `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `media` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `slug` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `status` int(10) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of movies
 -- ----------------------------
-INSERT INTO `movies` VALUES (5, 'BABA YAGA: ÁC QUỶ RỪNG SÂU', 'Svyatoslav Podgayevskiy', 'Svetlana Ustinova, Maryana Spivak, Alexey Rozin', 'Kinh Dị', '22/05/2020', '96 minutes', 'Tiếng Nga - Phụ đề tiếng Anh/Việt', 'C18 - PHIM CẤM KHÁN GIẢ DƯỚI 18 TUỔI', 'Lấy cảm hứng từ phù thủy Baba Yaga bắt cóc và ăn thịt trẻ con trong truyền thuyết dân gian, phim bắt đầu khi một gia đình trẻ chuyển đến căn hộ tại vùng ngoại ô. Một cô bảo mẫu được thuê về để chăm sóc bé gái mới sinh của gia đình. Ả nhanh chóng chiếm được cảm tình của mọi người trừ cậu con trai lớn Egor. Và rồi, một ngày nọ, khi trở về nhà, Egor hoảng hốt phát hiện em gái của mình đã biến mất cùng cô bảo mẫu đáng nghi. Ngạc nhiên thay, trước tình huống đó, bố mẹ cậu dường như không có chút ký ức nào về đứa con gái bé bỏng của họ. Egor bắt đầu tìm hiểu sự việc và phát hiện tấn bi kịch này bắt đầu từ nhân vật đáng sợ có tên Baba Yaga.', 'baba-yaga-ac-quy-rung-sau.png', 'https://www.youtube.com/embed/-Aa4ym5Me-I', 'baba-yaga-ac-quy-rung-sau');
-INSERT INTO `movies` VALUES (6, 'BÀ HOÀNG NÓI DỐI', 'Chang You-jeong', 'Ra Mi Ran, Na Moon Hee, Kim Mu Yeol,…', 'Hài', '15/05/2020', '105 phút', 'Tiếng Hàn - Phụ đề tiếng Việt', 'C16 - PHIM CẤM KHÁN GIẢ DƯỚI 16 TUỔI', 'Bộ phim xoay quanh câu chuyện của nữ nghị sĩ Joo Sang Sook với khả năng nói dối “như cơm bữa”, nhờ đó bà gặt hái được rất nhiều thành công trong sự nghiệp chính trị. Đột nhiên một ngày nọ, bà Joo không thể nói dối được nữa. Sự việc trở nên hết sức nguy cấp khi ngày tranh cử đã gần kề, liệu Joo Sang Sook có thành công đắc cử lần thứ tư hay không khi mọi lời bà nói ra đều là sự thật nghiệt ngã?', 'ba-hoang-noi-doi.png', 'https://www.youtube.com/embed/DMTVg0QtCUg', 'ba-hoang-noi-doi');
-INSERT INTO `movies` VALUES (7, 'ÁC MỘNG KINH HOÀNG', 'David Artur Clark', 'Nicola Lambo ( American Crime Story, Will & Grace), Hannah Ward (Ballistic, Sunny in the Dark), Conrad Goode (Paul Blart: Mall Cop, The Longest Yard), Micah Parker ( Mob City, Lola Versus)', 'Kinh Dị', '15/05/2020', '77 phút', 'Tiếng Anh - Phụ đề Tiếng Việt', 'C18 - PHIM CẤM KHÁN GIẢ DƯỚI 18 TUỔI', 'Sau cái chết bi thảm của con trai út, người mẹ và con gái trải qua những đêm dài kinh hoàng trong nỗi đau mất mát. Họ bắt đầu có dấu hiệu của chứng bệnh rối loạn giấc ngủ. Những ác mộng ngày càng dữ dội và dày đặc hơn...', 'ac-mong-kinh-hoang.png', 'https://www.youtube.com/embed/yffeet10TCM', 'ac-mong-kinh-hoang');
-INSERT INTO `movies` VALUES (8, 'TRUYỀN THUYẾT VỀ QUÁN TIÊN', 'Đinh Tuấn Vũ', 'Đỗ Thuý Hằng, Hồ Minh Khuê, Hoàng Mai Anh, Nguyễn Minh Hải, Leo Nguyễn, Trần Việt Hoàng, NSND. Quốc Trị, Lê Hoàng Long, Thế Nguyên…', 'Bí ẩn', '22/05/2020', '110 phút', 'Tiếng Việt - Phụ đề Tiếng Anh', 'C16 - PHIM CẤM KHÁN GIẢ DƯỚI 16 TUỔI', 'Ba cô gái xinh đẹp Mùi (Thúy Hằng), Phượng (Minh Khuê) và Tuyết Lan (Mai Anh) sống ở một hang động trong rừng Trường Sơn, nhận một nhiệm vụ đặc biệt là tiếp đón các anh &#34;lính lái xe&#34; tới nghỉ chân trong hang. Mỗi người một số phận, nhưng điểm chung là đều phải chịu đựng nỗi cô đơn tận cùng trong một hang sâu giữa rừng già mà không phải ai cũng hiểu và thấu cảm được.', 'truyen-thuyet-ve-quan-tien.png', 'https://www.youtube.com/embed/VLVoVPXMkMw', 'truyen-thuyet-ve-quan-tien');
-INSERT INTO `movies` VALUES (9, 'PHI VỤ ĐÀO TẨU', 'Francis Annan', 'Daniel Radcliffe,Daniel Webber, Mark Leonard Winter, Ian Hart', 'Hồi hộp', '09/05/2020', '106 phút', 'Tiếng Anh - Phụ đề Tiếng Việt', 'C13 - PHIM CẤM KHÁN GIẢ DƯỚI 13 TUỔI', 'Dựa trên câu chuyện có thật về cuộc đào tẩu thần kì khỏi nhà tù an ninh Pretoria (Nam Phi) của hai nhà hoạt động chính trị Tim Jerkins (Daniel Radcliffe) và Stephen Lee (Daniel Webber), bộ phim tái hiện chân thật và nghẹt thở kế hoạch vượt ngục phi thường và kịch tính. Nhờ vào óc quan sát tỉ mỉ, trí thông minh và sự khéo léo, họ đã tạo ra những chiếc chìa khóa gỗ vượt 10 cánh cửa thép và tìm lại tự do cho chính mình!', 'phi-vu-dao-tau.png', 'https://www.youtube.com/embed/NmhFX7whhhI', 'phi-vu-dao-tau');
+INSERT INTO `movies` VALUES (5, 'BABA YAGA: ÁC QUỶ RỪNG SÂU', 'Svyatoslav Podgayevskiy', 'Svetlana Ustinova, Maryana Spivak, Alexey Rozin', 'Kinh Dị', '22/05/2020', '96 minutes', 'Tiếng Nga - Phụ đề tiếng Anh/Việt', 'C18 - PHIM CẤM KHÁN GIẢ DƯỚI 18 TUỔI', 'Lấy cảm hứng từ phù thủy Baba Yaga bắt cóc và ăn thịt trẻ con trong truyền thuyết dân gian, phim bắt đầu khi một gia đình trẻ chuyển đến căn hộ tại vùng ngoại ô. Một cô bảo mẫu được thuê về để chăm sóc bé gái mới sinh của gia đình. Ả nhanh chóng chiếm được cảm tình của mọi người trừ cậu con trai lớn Egor. Và rồi, một ngày nọ, khi trở về nhà, Egor hoảng hốt phát hiện em gái của mình đã biến mất cùng cô bảo mẫu đáng nghi. Ngạc nhiên thay, trước tình huống đó, bố mẹ cậu dường như không có chút ký ức nào về đứa con gái bé bỏng của họ. Egor bắt đầu tìm hiểu sự việc và phát hiện tấn bi kịch này bắt đầu từ nhân vật đáng sợ có tên Baba Yaga.', 'baba-yaga-ac-quy-rung-sau.png', 'https://www.youtube.com/embed/-Aa4ym5Me-I', 'baba-yaga-ac-quy-rung-sau', 1);
+INSERT INTO `movies` VALUES (6, 'BÀ HOÀNG NÓI DỐI', 'Chang You-jeong', 'Ra Mi Ran, Na Moon Hee, Kim Mu Yeol,…', 'Hài', '15/05/2020', '105 phút', 'Tiếng Hàn - Phụ đề tiếng Việt', 'C16 - PHIM CẤM KHÁN GIẢ DƯỚI 16 TUỔI', 'Bộ phim xoay quanh câu chuyện của nữ nghị sĩ Joo Sang Sook với khả năng nói dối “như cơm bữa”, nhờ đó bà gặt hái được rất nhiều thành công trong sự nghiệp chính trị. Đột nhiên một ngày nọ, bà Joo không thể nói dối được nữa. Sự việc trở nên hết sức nguy cấp khi ngày tranh cử đã gần kề, liệu Joo Sang Sook có thành công đắc cử lần thứ tư hay không khi mọi lời bà nói ra đều là sự thật nghiệt ngã?', 'ba-hoang-noi-doi.png', 'https://www.youtube.com/embed/DMTVg0QtCUg', 'ba-hoang-noi-doi', 1);
+INSERT INTO `movies` VALUES (7, 'ÁC MỘNG KINH HOÀNG', 'David Artur Clark', 'Nicola Lambo ( American Crime Story, Will & Grace), Hannah Ward (Ballistic, Sunny in the Dark), Conrad Goode (Paul Blart: Mall Cop, The Longest Yard), Micah Parker ( Mob City, Lola Versus)', 'Kinh Dị', '15/05/2020', '77 phút', 'Tiếng Anh - Phụ đề Tiếng Việt', 'C18 - PHIM CẤM KHÁN GIẢ DƯỚI 18 TUỔI', 'Sau cái chết bi thảm của con trai út, người mẹ và con gái trải qua những đêm dài kinh hoàng trong nỗi đau mất mát. Họ bắt đầu có dấu hiệu của chứng bệnh rối loạn giấc ngủ. Những ác mộng ngày càng dữ dội và dày đặc hơn...', 'ac-mong-kinh-hoang.png', 'https://www.youtube.com/embed/yffeet10TCM', 'ac-mong-kinh-hoang', 0);
+INSERT INTO `movies` VALUES (8, 'TRUYỀN THUYẾT VỀ QUÁN TIÊN', 'Đinh Tuấn Vũ', 'Đỗ Thuý Hằng, Hồ Minh Khuê, Hoàng Mai Anh, Nguyễn Minh Hải, Leo Nguyễn, Trần Việt Hoàng, NSND. Quốc Trị, Lê Hoàng Long, Thế Nguyên…', 'Bí ẩn', '22/05/2020', '110 phút', 'Tiếng Việt - Phụ đề Tiếng Anh', 'C16 - PHIM CẤM KHÁN GIẢ DƯỚI 16 TUỔI', 'Ba cô gái xinh đẹp Mùi (Thúy Hằng), Phượng (Minh Khuê) và Tuyết Lan (Mai Anh) sống ở một hang động trong rừng Trường Sơn, nhận một nhiệm vụ đặc biệt là tiếp đón các anh &#34;lính lái xe&#34; tới nghỉ chân trong hang. Mỗi người một số phận, nhưng điểm chung là đều phải chịu đựng nỗi cô đơn tận cùng trong một hang sâu giữa rừng già mà không phải ai cũng hiểu và thấu cảm được.', 'truyen-thuyet-ve-quan-tien.png', 'https://www.youtube.com/embed/VLVoVPXMkMw', 'truyen-thuyet-ve-quan-tien', 0);
+INSERT INTO `movies` VALUES (9, 'PHI VỤ ĐÀO TẨU', 'Francis Annan', 'Daniel Radcliffe,Daniel Webber, Mark Leonard Winter, Ian Hart', 'Hồi hộp', '09/05/2020', '106 phút', 'Tiếng Anh - Phụ đề Tiếng Việt', 'C13 - PHIM CẤM KHÁN GIẢ DƯỚI 13 TUỔI', 'Dựa trên câu chuyện có thật về cuộc đào tẩu thần kì khỏi nhà tù an ninh Pretoria (Nam Phi) của hai nhà hoạt động chính trị Tim Jerkins (Daniel Radcliffe) và Stephen Lee (Daniel Webber), bộ phim tái hiện chân thật và nghẹt thở kế hoạch vượt ngục phi thường và kịch tính. Nhờ vào óc quan sát tỉ mỉ, trí thông minh và sự khéo léo, họ đã tạo ra những chiếc chìa khóa gỗ vượt 10 cánh cửa thép và tìm lại tự do cho chính mình!', 'phi-vu-dao-tau.png', 'https://www.youtube.com/embed/NmhFX7whhhI', 'phi-vu-dao-tau', 1);
 
 -- ----------------------------
 -- Table structure for seat_price
@@ -294,3 +302,5 @@ INSERT INTO `show_times` VALUES (5, '17:00 PM', 1700, '18:20 PM');
 INSERT INTO `show_times` VALUES (6, '18:30 PM', 1830, '19:50 PM');
 INSERT INTO `show_times` VALUES (7, '20:00 PM', 2000, '21:20 PM');
 INSERT INTO `show_times` VALUES (8, '21:30 PM', 2130, '22:00 PM');
+
+SET FOREIGN_KEY_CHECKS = 1;
